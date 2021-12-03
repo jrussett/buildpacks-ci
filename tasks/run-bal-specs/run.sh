c567b6ea1e037bbbe5f8021584bcffad43bb7331
Josh Russett <jrussett@vmware.com>
2021-12-03 22:09:26 +0000

    wip: trying to install ginkgo properly

--- tasks/run-bal-specs/run.sh
+++ tasks/run-bal-specs/run.sh
@@ -3,8 +3,13 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIR="$( realpath "${SCRIPT_DIR}/../../.." )"
+GO_BIN="${ROOT_DIR}/go/bin"
+PATH="${GO_BIN}:${PATH}"
 
-go get -u github.com/onsi/ginkgo/ginkgo
+mkdir -p "${GO_BIN}"
+go install github.com/onsi/ginkgo/ginkgo@latest
 
-cd "${SCRIPT_DIR}/buildpackapplifecycle"
+
+cd "${ROOT_DIR}/bal-develop"
 ginkgo -r
